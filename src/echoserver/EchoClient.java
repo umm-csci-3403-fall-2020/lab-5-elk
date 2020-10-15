b4f84e41764bea72a5203257b20881b1db8a9fe8
oswoo003 <oswoo003@umn.edu>
2020-10-15 10:53:46 -0500

    First Pass at server and client. Cannot get bats testing to run at all.

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,4 +1,44 @@
 package echoserver;
 
+import java.io.*;
+import java.net.*;
+
 public class EchoClient {
+    public static final int portNumber = 6013;
+
+    public static void main(String[] args) throws IOException {
+        String server;
+        if (args.length == 0) {
+            server = "127.0.0.1";
+        } else {
+            server = args[0];
+        }
+
+        try {
+            Socket socket = new Socket(server, portNumber);
+
+            InputStream input = socket.getInputStream();
+            OutputStream output = socket.getOutputStream();
+
+            InputStreamReader keyboard = new InputStreamReader(System.in);
+
+            int k = keyboard.read();
+            int i;
+
+            while (k != -1) {
+                output.write(k);
+                i = input.read();
+                System.out.print(i);
+                k = keyboard.read();
+            }
+
+            socket.close();
+        } catch (ConnectException ce) {
+            System.out.println("We were unable to connect to " + server);
+            System.out.println("You should make sure the server is running.");
+        } catch (IOException ioe) {
+            System.out.println("We caught an unexpected exception");
+            System.err.println(ioe);
+        }
+    }
 }
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,32 @@
 package echoserver;
+import java.net.*;
+import java.io.*;
 
 public class EchoServer {
+    public static final int portNumber = 6013;
+
+    public static void main(String[] args) {
+        try{
+            ServerSocket sock = new ServerSocket(portNumber);
+
+            while (true) {
+                Socket client = sock.accept();
+                System.out.println("Got a request!");
+
+                InputStream input = client.getInputStream();
+                OutputStream output = client.getOutputStream();
+
+                int i = input.read();
+
+                while (i != -1) {
+                    output.write(i);
+                    i = input.read();
+                }
+            }
+
+        } catch (IOException ioe) {
+            System.out.println("We caught an unexpected exception");
+            System.err.println(ioe);
+        }
+    }
 }
